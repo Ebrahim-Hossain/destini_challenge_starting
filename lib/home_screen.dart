b969549449a09ae636eb5e6cb3bf28940b3bf8da
Ebrahim Hossain <ebrahimhossain626@gmail.com>
2025-08-20 22:30:18 +0600

    text added

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,7 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
+          image: DecorationImage(fit: BoxFit.fill,
               image: AssetImage('image/background.png'),
           ),
         ),
@@ -43,10 +43,14 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               Expanded(
                 flex: 2,
-                child: InkWell(
-                  onTap: () {
+                child: TextButton(
+                  onPressed: () {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(choiceNumber: 1);
+                    });
+
                   },
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
@@ -60,10 +64,13 @@ class _HomeScreenState extends State<HomeScreen> {
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: InkWell(
-                  onTap: () {
+                child: TextButton(
+                  onPressed: () {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    setState(() {
+                      storyBrain.nextStory(choiceNumber: 2);
+                    });
                   },
 
                   child: Text(
